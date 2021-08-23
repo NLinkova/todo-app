b2ec19618ac00e8187f9270cf5b2c8d1d10d0576
Natalia Linkova <linkova1109@yandex.ru>
2021-08-23 15:07:58 +1000

    fetch

--- app/interactivity/app.js
+++ app/interactivity/app.js
@@ -1,1 +1,15 @@
 console.log('Todo app');
+
+function login (evt) {
+    evt.preventDefault ();
+    method = {method: evt.target.method};
+    fetch(evt.target.action, method)
+       
+}
+
+function login (evt) {
+    evt.preventDefault ();
+    method = {method: evt.target.method};
+    fetch(evt.target.action, method)
+       
+}
--- server.js
+++ server.js
@@ -3,9 +3,54 @@ const app = express()
 
 app.use('/', express.static('app')) 
   
+// authentification
+app.post('/api/login', function (req, res) {
+    res.send('Test login')
+})
 
-app.get('/api', function (req, res) {
+app.put('/api/register', function (req, res) {
+    res.send('Test register')
+})
+
+app.get('/api/isLoggedin', function (req, res) {
+    res.send('Test isLoggedin')
+})
+
+// todo categories CRUD
+app.get('/api/getcat', function (req, res) {
+    res.send('Test get Categories')
+})
+
+app.put('/api/addcat', function (req, res) {
+    res.send('Test add Categories')
+})
+
+app.delete('/api/delcat/:catid', function (req, res) {
+    res.send('Test delete Categories')
+})
+
+app.patch('/api/updatecat/:catid', function (req, res) {
+    res.send('Test update Categories')
+})
+
+//todo list CRUD
+app.get('/api/gettodos/:catid', function (req, res) {
     res.send('Test API')
 })
+
+app.put('/api/addtodo', function (req, res) {
+    res.send('Test API')
+})
+
+app.delete('/api/deltodo/:todoid', function (req, res) {
+    res.send('Test API')
+})
+
+app.patch('/api/updatetodo/:todoid', function (req, res) {
+    res.send('Test API')
+})
+
+
+
 
 app.listen(3000)
